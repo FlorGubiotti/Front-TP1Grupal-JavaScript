4d7b3900b39cbf0aec0e160460c48e64581a4c97
FlorGubiotti <florgubiotti@gmail.com>
2024-04-09 23:53:57 -0300

    detalle

--- detalle.js
+++ detalle.js
@@ -1,8 +1,23 @@
-(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const idNoticia = urlParams.get("id");
+const loadDetails = () => {
 
-    fetch(`http://localhost:8080/noticias/buscar/${idNoticia}`)
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get("id");
+    console.log(id);
+
+    fetch("http://localhost:8080/empresas/buscar/" + id)
+    .then(async (res) => {
+        const responseEmpresa = await res.json();
+
+        document.getElementById("telefono").innerHTML = responseEmpresa.telefono;
+        document.getElementById("horario").innerHTML = responseEmpresa.horarioAtencion;
+        document.getElementById("denominacion-footer").innerHTML = responseEmpresa.denominacion;
+        document.getElementById("denominacion-header").innerHTML = responseEmpresa.denominacion;
+    })
+    .catch((err) => {
+        console.error("Error al cargar los datos de la empresa:", err);
+    });
+
+    fetch("http://localhost:8080/noticias/buscar/" + id)
         .then(async (res) => {
             const response = await res.json();
 
@@ -10,31 +25,16 @@
 
             document.getElementById("tituloNoticia").innerHTML = response.tituloNoticia;
             document.getElementById("imagenNoticia").src = response.imagenNoticia;
-			document.getElementById("tituloNoticia-content").innerHTML = response.tituloNoticia;
+            document.getElementById("tituloNoticia-content").innerHTML = response.tituloNoticia;
             document.getElementById("fechaPublicacion").innerHTML = response.fechaPublicacion;
             document.getElementById("resumenNoticia").innerHTML = response.resumenNoticia;
             document.getElementById("contenidoHTML").innerHTML = response.contenidoHTML;
 
-            const idEmpresa = response.idEmpresa;
-			console.log(idEmpresa)
-
-            fetch(`http://localhost:8080/empresas/buscar/${idEmpresa}`)
-                .then(async (resEmpresa) => {
-                    const responseEmpresa = await resEmpresa.json();
-
-                    console.log(responseEmpresa);
-
-                    document.getElementById("telefono").innerHTML = responseEmpresa.telefono;
-                    document.getElementById("horario").innerHTML = responseEmpresa.horarioAtencion;
-                    document.getElementById("denominacion-footer").innerHTML = responseEmpresa.denominacion;
-                    document.getElementById("denominacion-header").innerHTML = responseEmpresa.denominacion;
-                })
-                .catch((err) => {
-                    console.error("Error al cargar los datos de la empresa:", err);
-                });
         })
         .catch((err) => {
-            console.error("Error al cargar los datos de la noticia:", err);
-        });
 
-})();
+        })
+
+   
+
+}
